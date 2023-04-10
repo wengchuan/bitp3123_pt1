8353683ec0aae3a5f1e06ccd95f3d1c2d65c4d3f
wengchuan <wengchuankot@gmail.com>
2023-04-11 00:10:23 +0800

    Add new class Location

---
+++ samples/src/demo/Location.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Location {
+		private String place;
+		public void setPlace(String place) {
+			this.place = place;
+		}
+		public String getPlace() {
+			return this.place;
+		}
+	
+}
